3bcd1d4db52b5e5be6f2f992f7b6606814ffbdc3
wuyuliang <2331336537@qq.com>
2024-04-08 16:21:21 +0800

    fix: 登录过期修复

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,5 +1,5 @@
 import { message } from "antd";
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: 'http://rain-d.xyz/api',
@@ -7,73 +7,71 @@ export const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(function (config) {
-    const accessToken = localStorage.getItem('access_token');
+  const accessToken = localStorage.getItem("access_token");
 
-    if(accessToken) {
-        config.headers.authorization = 'Bearer ' + accessToken;
-    }
-    return config;
-})
+  if (accessToken) {
+    config.headers.authorization = "Bearer " + accessToken;
+  }
+  return config;
+});
 
 interface PendingTask {
-    config: AxiosRequestConfig
-    resolve: Function
-  }
+  config: AxiosRequestConfig;
+  resolve: Function;
+}
 let refreshing = false;
 const queue: PendingTask[] = [];
 
 axiosInstance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error) => {
-        let { data, config } = error.response;
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    console.log(error);
+    let { data, config } = error.response;
 
-        if(refreshing) {
-            return new Promise((resolve) => {
-                queue.push({
-                    config,
-                    resolve
-                });
-            });
-        }
-
-        if (data.code === 401 && !config.url.includes('/user/refresh')) {
-            
-            refreshing = true;
-
-            const res = await refreshToken();
-
-            refreshing = false;
-
-            if(res.status === 200) {
-
-                queue.forEach(({config, resolve}) => {
-                    resolve(axiosInstance(config))
-                })
-
-                return axiosInstance(config);
-            } else {
-                message.error(res.data);
-
-                setTimeout(() => {
-                    window.location.href = '/login';
-                }, 1500);
-            }
-            
-        } else {
-            return error.response;
-        }
+    if (refreshing && !config.url.includes("/user/refresh")) {
+      return new Promise((resolve) => {
+        queue.push({
+          config,
+          resolve,
+        });
+      });
     }
-)
+
+    if (data.code === 401 && !config.url.includes("/user/refresh")) {
+      refreshing = true;
+      let res = await refreshToken();
+
+      refreshing = false;
+      if (res.status === 200) {
+        queue.forEach(({ config, resolve }) => {
+          resolve(axiosInstance(config));
+        });
+
+        return axiosInstance(config);
+      } else {
+        message.error(res.data);
+
+        setTimeout(() => {
+          window.location.href = "/login";
+        }, 1500);
+      }
+    } else {
+      
+      return error.response;
+    }
+  }
+);
 
 async function refreshToken() {
-    const res = await axiosInstance.get('/user/refresh', {
-        params: {
-          refresh_token: localStorage.getItem('refresh_token')
-        }
-    });
-    localStorage.setItem('access_token', res.data.access_token || '');
-    localStorage.setItem('refresh_token', res.data.refresh_token || '');
-    return res;
+  const res = await axiosInstance.get("/user/refresh", {
+    params: {
+      refresh_token: localStorage.getItem("refresh_token"),
+    },
+  });
+  localStorage.setItem("access_token", res.data.access_token || "");
+  localStorage.setItem("refresh_token", res.data.refresh_token || "");
+  return res;
 }
+
