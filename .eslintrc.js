c638d85c661dd1d8d87150be63ef5f0621b5c38d
rain <2331336537@qq.com>
2024-07-21 12:27:49 +0800

    feat: 接入google登录

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // 其他配置
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off'
+  },
+};
--- src/page/index/index.tsx
+++ src/page/index/index.tsx
@@ -2,10 +2,26 @@ import { UserOutlined } from "@ant-design/icons";
 import { Outlet, Link } from "react-router-dom";
 import "./index.css";
 import { useEffect, useState } from "react";
+import cookies from "js-cookie";
 
 export function Index() {
   const [headPic, setHeadPic] = useState();
 
+  useEffect(() => {
+    const userInfo = cookies.get("userinfo");
+    const accessToken = cookies.get("accessToken");
+    const refreshToken = cookies.get("refreshToken");
+
+    if (userInfo && accessToken && refreshToken) {
+      localStorage.setItem("user_info", userInfo);
+      localStorage.setItem("access_token", accessToken);
+      localStorage.setItem("refresh_token", refreshToken);
+
+      cookies.remove("userInfo");
+      cookies.remove("accessToken");
+      cookies.remove("refreshToken");
+    }
+  }, []);
   useEffect(() => {
     const userInfo = localStorage.getItem("user_info");
     if (userInfo) {
